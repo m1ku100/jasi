11219e74f02ad1ff6d4a2f995663ddc8512cc2a7
m1ku100 <ilham.puji100@gmail.com>
2018-05-29 19:53:59 +0700

    Minor update profile

--- resources/views/public/include/profile.blade.php
+++ resources/views/public/include/profile.blade.php
@@ -16,23 +16,7 @@
     </header>
 
     <div id="fh5co-contact">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-5 col-md-push-1 animate-box">
 
-                    <div class="fh5co-contact-info">
-                        <h3>Contact Information</h3>
-                        <ul>
-                            <li class="address">Jalan Ketintang, <br> Universitas Negeri Surabaya</li>
-                            <li class="phone"><a href="tel://1234567920">+62 xxx xxxx xxxx</a></li>
-                            <li class="email"><a href="mailto:info@yoursite.com">jasi@service.com</a></li>
-                            <li class="url"><a href="http://gettemplates.co">jasi.co.id</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-        </div>
         <div class="container">
             <div class="row">
                 <div class="col-md-6 animate-box">
@@ -43,15 +27,18 @@
                         <div class="row form-group">
                             <div class="col-md-12">
                                 <label for="fname">First Name</label>
-                                <input type="text" id="fname" class="form-control" placeholder="Your firstname" value="{{ Auth::user()->name }}" name="name">
+                                <input type="text" id="fname" class="form-control" placeholder="Your firstname"
+                                       value="{{ Auth::user()->name }}" name="name">
                             </div>
                         </div>
 
                         <div class="row form-group">
                             <div class="col-md-12">
                                 <label for="email">Email</label>
-                                <input type="text" id="email" class="form-control" value="{{ Auth::user()->email }}" placeholder="Your email address" name="email">
-                                <input type="hidden" id="email" class="form-control" value="{{ Auth::user()->id }}" placeholder="Your email address" name="id">
+                                <input type="text" id="email" class="form-control" value="{{ Auth::user()->email }}"
+                                       placeholder="Your email address" name="email">
+                                <input type="hidden" id="email" class="form-control" value="{{ Auth::user()->id }}"
+                                       placeholder="Your email address" name="id">
                             </div>
                         </div>
                         <div class="form-group">
@@ -63,24 +50,28 @@
                 <div class="col-md-6 animate-box">
                     <h3>Setting Password</h3>
                     <form action="{{route('save.pass')}}"
-                    method="post">
-                    {{ csrf_field() }}
+                          method="post">
+                        {{ csrf_field() }}
                         <div class="row form-group">
                             <div class="col-md-12">
                                 <label for="fname">Old Password</label>
-                                <input type="password" id="fname" class="form-control" placeholder="Your Old Password"  name="passlama">
+                                <input type="password" id="fname" class="form-control" placeholder="Your Old Password"
+                                       name="passlama">
                             </div>
                         </div>
 
                         <div class="row form-group">
                             <div class="col-md-6">
                                 <label for="email">New Password</label>
-                                <input type="password" id="email" class="form-control"  placeholder="New Password" name="passbaru">
-                                <input type="hidden" id="email" class="form-control" value="{{ Auth::user()->id }}" placeholder="Your email address" name="id">
+                                <input type="password" id="email" class="form-control" placeholder="New Password"
+                                       name="passbaru">
+                                <input type="hidden" id="email" class="form-control" value="{{ Auth::user()->id }}"
+                                       placeholder="Your email address" name="id">
                             </div>
                             <div class="col-md-6">
                                 <label for="email">Re-Type Password</label>
-                                <input type="password" id="email" class="form-control"  placeholder="Re-type New Password" name="pass_confirm">
+                                <input type="password" id="email" class="form-control"
+                                       placeholder="Re-type New Password" name="pass_confirm">
                             </div>
                         </div>
                         <div class="form-group">
@@ -89,6 +80,25 @@
 
                     </form>
                 </div>
+            </div>
+
+        </div>
+        <div class="container">
+            <div class="row">
+                <h3>Order History</h3>
+                @foreach(App\Order::where('user_id','=', Auth::user()->id)->get() as $histor )
+                    <div class="col-md-4 animate-box">
+                        <div class="fh5co-contact-info">
+                            <ul>
+                                <li class="address"><b>Dari</b> , <br> {{$histor->pengambilan}}</li>
+                                <li class="address"><b>Menuju</b> , <br> {{$histor->tujuan}}</li>
+                                <li class="email">RP. {{$histor->harga}}   </li>
+                                <li class="url">{{$histor->updated_at}}</li>
+                            </ul>
+
+                        </div>
+                    </div>
+                @endforeach
             </div>
 
         </div>
